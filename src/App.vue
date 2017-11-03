2b7e498ec10f43a808f4bbfe724369c954b349b1
Vladimir Pavković <vladimirpavk@gmail.com>
2017-11-03 14:10:16 +0100

    to tutorial 24

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>{{app_name}}</h1>
+    <p>{{ app_dsc() }}</p>
+    <proizvodi v-bind:lista_proizvoda="lista_proizvoda" v-bind:neki_objekt="neki_objekt" ulazni_string="Nesto"></proizvodi>
+    <proizvodi v-bind:lista_proizvoda="lista_proizvoda" v-bind:neki_objekt="neki_objekt"></proizvodi>
+    <!--<heder vi-bind:dodatak="dodatakTata"></heder>
+    <futer vi-bind:dodatak="dodatakTata"></futer>-->
   </div>
 </template>
 
 <script>
+import Products from './Products.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+
 export default {
-  name: 'app',
+  components: {
+    'proizvodi': Products,
+    'heder': Header,
+    'futer': Footer
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      app_name: "My Vue App Name",
+      dodatakTata: "Dodatak tata...",
+      lista_proizvoda: ['Sir', 'Mleko', 'Meso', 'Hleba'],
+      neki_objekt:{
+        ime: "Vladimir",
+        prezime: "Pavković"
+      }
+    }
+  },
+  methods:{
+    app_dsc(){
+      return "My App Description from function"
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/Products.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="proizvod in lista_proizvoda">{{ proizvod }}</li>
+        </ul>
+        <p>{{neki_objekt.ime}} {{neki_objekt.prezime}}</p>
+        <div>
+                <p>{{ulazni_string}}</p>
+        </div>
+        <button v-on:click="button1_Clicked">Pročitaj objekat</button>
+    </div>
+</template>
+<script>
+    export default{
+        props:{
+            lista_proizvoda: {
+                type: Array,
+                required: true
+            },
+            neki_objekt: {
+                type: Object,
+                required: true
+            },
+            ulazni_string: {
+                type: String
+            }
+        },
+        data() {
+            return {
+            }
+        },
+        methods:{
+            button1_Clicked(){
+                console.log(this.neki_objekt);
+                this.neki_objekt.ime="Nikola";
+            }
+        },
+        created(){
+            
+        }
+    }
+</script>
