814e9c4ca705adb05bb3570d6eedbda2f3e71659
Vladimir Pavković <vladimirpavk@gmail.com>
2017-11-08 13:18:00 +0100

    vezbanje

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,25 @@
   <div>
     <component v-bind:is="cur_component"></component>
     <button v-on:click="button1Clicked">Change current component</button>
+    <hr>
+
+    <label for="checkBox1">Vrednost 1</label>
+    <input type="checkbox" value="Vrednost1" id="checkBox1" v-model="checkBoxValues"/>
+
+    <label for="checkBox2">Vrednost 2</label>
+    <input type="checkbox" value="Vrednost2" id="checkBox2" v-model="checkBoxValues"/>
+
+    <label for="checkBox1">Vrednost 3</label>
+    <input type="checkbox" value="Vrednost3" id="checkBox3" v-model="checkBoxValues"/>
+
+    <hr>
+
+    {{checkBoxValues}}
+    
+    <hr>
+
+    <button v-on:click="getPosts">Get Posts</button>
+
   </div>
 </template>
 
@@ -16,7 +35,8 @@ export default {
     },
     data(){
       return{
-        cur_component: 'my_header'
+        cur_component: 'my_header',
+        checkBoxValues: []
       }
     },
     methods:{
@@ -28,6 +48,13 @@ export default {
         {
           this.cur_component = 'my_header'
         }
+      },
+      getPosts(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
+          console.log(data)
+        }).catch((error)=>{
+          console.log(error)
+        })
       }
     }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 export const eventBus=new Vue();
 
